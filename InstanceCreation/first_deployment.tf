f0b6bf3ce38ed04b86891e39893caf62fb92feac
Sandeepkr93 <58113378+Sandeepkr93@users.noreply.github.com>
2020-02-13 15:27:23 +0530

    Update first_deployment.tf
    changes

--- InstanceCreation/first_deployment.tf
+++ InstanceCreation/first_deployment.tf
@@ -1,9 +1,35 @@
-provider "aws" {
-  access_key = "AKIAYKBCL35Z4D6JMLFS"
-  secret_key = "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"
-  region     = "us-west-2"
+terraform {
+  backend "local" {
+    path = "/tmp/terraform/workspace/terraform.tfstate"
+  }
+
 }
-resource "aws_instance" "example" {
-  ami           = "ami-0994c095691a46fb5"
-  instance_type = "t2.micro"
+
+provider "aws" {
+  region = "us-east-2"
+  
+}
+
+resource "aws_instance" "backend" {
+  ami                    = "ami-08cec7c429219e339"
+  instance_type          = "t2.micro"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.sg-id}"]
+
+}
+
+resource "null_resource" "remote-exec-1" {
+    connection {
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    host        = "${aws_instance.backend.public_ip}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install python sshpass -y",
+    ]
+  }
 }
